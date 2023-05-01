ed09c5b66eba798d83c4979b7c3405f31fa3fa1f
Maouai233 <Maouai233@outlook.com>
2023-05-02 07:46:25 +0800

    [bili] 006 文件打开及读写、定位

--- Src/Boot/SigmaBootPkg/File.c
+++ Src/Boot/SigmaBootPkg/File.c
@@ -56,3 +56,115 @@ EFI_STATUS InitializeFileServices ()
 
     return Status;
 }
+
+EFI_STATUS FileOpen (
+        IN      CHAR16            *Path,
+        IN      UINT64            Mode,
+           OUT  EFI_FILE_PROTOCOL **File
+    )
+{
+    EFI_STATUS Status = gFileProtocol->Open (
+            gFileProtocol,
+            File,
+            Path,Mode,
+            0
+        );
+    if (EFI_ERROR(Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Open File : %S - Status : %r\n",Path,Status));
+        return Status;
+    }
+    DEBUG ((DEBUG_INFO ,"[ OK ] Open File : %S\n",Path));
+
+    return Status;
+}
+
+EFI_STATUS FileRead (
+        IN     EFI_FILE_PROTOCOL *File,
+           OUT VOID              *Data,
+        IN OUT UINTN             *Size
+        )
+{
+    EFI_STATUS Status = File->Read (
+            File,
+            Size,
+            Data
+        );
+    if (EFI_ERROR (Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Read File - Size later : %llu,Status : %r\n",*Size,Status));
+        return Status;
+    }
+    DEBUG ((DEBUG_INFO ,"[ OK ] Read File - Siz : %llu\n",*Size));
+
+    return Status;
+}
+
+EFI_STATUS FileWrite (
+        IN     EFI_FILE_PROTOCOL *File,
+        IN     VOID              *Buffer,
+        IN OUT UINTN             *Size
+        )
+{
+    EFI_STATUS Status = File->Write (
+            File,
+            Size,
+            Buffer
+        );
+    if (EFI_ERROR (Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Write File - Status : %r\n",Status));
+        return Status;
+    }
+    DEBUG ((DEBUG_INFO ,"[ OK ] Write File - Size : %llu\n",*Size));
+
+    return Status;
+}
+
+EFI_STATUS FileFlush (EFI_FILE_PROTOCOL *File)
+{
+    EFI_STATUS Status = File->Flush (File);
+
+    if (EFI_ERROR(Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Flush File - Status : %r\n",Status));
+        return Status;
+    }
+    DEBUG ((DEBUG_INFO ,"[ OK ] Flush file\n"));
+
+    return Status;
+}
+
+EFI_STATUS FileSetPosition (
+        IN EFI_FILE_PROTOCOL    *File,
+        IN UINT64               Position
+    )
+{
+    EFI_STATUS Status = File->SetPosition (File,Position);
+    if (EFI_ERROR(Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Set Position - Pos : %llu - Status : %r\n",Position,Status));
+        return Status;
+    }
+    DEBUG ((DEBUG_INFO ,"[ OK ] Set Position - Pos : %llu\n",Position));
+
+    return Status;
+}
+
+UINT64 FileGetPosition (
+        IN EFI_FILE_PROTOCOL *File
+        )
+{
+    UINT64 Position = 0;
+
+    EFI_STATUS Status = File->GetPosition (File,&Position);
+    if (EFI_ERROR(Status))
+    {
+        DEBUG ((DEBUG_INFO, "[FAIL] Failed to get position of the ptr of this file - Status : %r\n",Status));
+        return 0;
+    }
+    DEBUG ((DEBUG_INFO,"[ OK ] Got position of this file - Pos : %llu\n",Position));
+
+    return Position;
+}
+
--- Src/Boot/SigmaBootPkg/Main.c
+++ Src/Boot/SigmaBootPkg/Main.c
@@ -1,5 +1,7 @@
 #include <Uefi.h>
 
+#include <Library/UefiLib.h>
+
 #include <Boot/Boot.h>
 #include <Boot/Graphics.h>
 #include <Boot/File.h>
@@ -14,5 +16,18 @@ EFI_STATUS EFIAPI UefiMain (
     InitializeGraphicsServices();
     InitializeFileServices();
 
+    EFI_FILE_PROTOCOL *File;
+    FileOpen (L"\\Read.txt",O_READ | O_WRITE | O_CREATE,&File);
+
+    CHAR8 *Buffer = "Hello world!!!";
+    UINTN Size = AsciiStrLen (Buffer);
+
+    FileWrite (File,Buffer,&Size);
+    Print (L"Write file!\n");
+
+    FileRead (File,Buffer,&Size);
+    Print (L"Read file and get data! - %a\n",Buffer);
+
     return EFI_SUCCESS;
 }
+
