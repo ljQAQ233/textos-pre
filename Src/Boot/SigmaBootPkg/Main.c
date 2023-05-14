9038a5e5a970d38daed0f6f99e3709d8efa0c2a0
Maouai233 <Maouai233@outlook.com>
2023-05-14 13:49:11 +0800

    [bili] 011 Bmp位图显示          -> Also added the introduction of ClearScreen().

--- Src/Boot/SigmaBootPkg/Main.c
+++ Src/Boot/SigmaBootPkg/Main.c
@@ -17,31 +17,14 @@ EFI_STATUS EFIAPI UefiMain (
     InitializeGraphicsServices();
     InitializeFileServices();
 
-    /* Blue 0 Green 0 Red 0 -> RED */
-    EFI_GRAPHICS_OUTPUT_BLT_PIXEL Template = { 0, 0, 255, 0 };
-    gGraphicsOutputProtocol->Blt (
-            gGraphicsOutputProtocol,
-            &Template, EfiBltVideoFill,
-            0, 0, 0, 0,
-            16, 16,
-            0
-        );
-
-    EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer = AllocatePool (16 * 16 * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
-    gGraphicsOutputProtocol->Blt (
-            gGraphicsOutputProtocol,
-            BltBuffer, EfiBltVideoToBltBuffer,
-            0, 0, 0, 0,
-            16, 16,
-            16
-        );
-
-    /* Hard Core!!! */
-    EFI_GRAPHICS_OUTPUT_BLT_PIXEL *Ptr = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)gGraphicsOutputProtocol->Mode->FrameBufferBase;
-    for (int i = 0 ; i < gGraphicsOutputProtocol->Mode->FrameBufferSize / sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL) ; i++, Ptr++)
-    {
-        *Ptr = Template;
-    }
+    GraphicsBmpDisplay (L"\\Sigma1.bmp" , 0, 0);
+    SystemTable->ConOut->ClearScreen (SystemTable->ConOut);
+    GraphicsBmpDisplay (L"\\Sigma4.bmp" , 0, 0);
+    SystemTable->ConOut->ClearScreen (SystemTable->ConOut);
+    GraphicsBmpDisplay (L"\\Sigma8.bmp" , 0, 0);
+    SystemTable->ConOut->ClearScreen (SystemTable->ConOut);
+    GraphicsBmpDisplay (L"\\Sigma24.bmp", 0, 0);
+    SystemTable->ConOut->ClearScreen (SystemTable->ConOut);
 
     return EFI_SUCCESS;
 }
