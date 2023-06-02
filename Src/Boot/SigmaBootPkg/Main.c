21135beaa3ed17dce2f945024a903b963d8d67b6
Maouai233 <Maouai233@outlook.com>
2023-06-03 00:35:28 +0800

    [bili] 017 退出启动时服务

--- Src/Boot/SigmaBootPkg/Main.c
+++ Src/Boot/SigmaBootPkg/Main.c
@@ -1,4 +1,5 @@
 #include <Uefi.h>
+#include <Library/UefiBootServicesTableLib.h>
 
 #include <Library/UefiLib.h>
 #include <Library/MemoryAllocationLib.h>
@@ -8,7 +9,38 @@
 #include <Boot/File.h>
 #include <Boot/Config.h>
 #include <Boot/Graphics.h>
+#include <Boot/Memory.h>
 #include <Boot/Kernel.h>
+
+/* From tanyugang's Code,and I modified it,very thanks! */
+
+EFI_STATUS ExitBootServices (
+        IN     EFI_HANDLE ImageHandle,
+           OUT MAP_INFO   *Info
+    )
+{
+    EFI_STATUS Status = EFI_SUCCESS;
+
+    Status = MemoryGetMap (Info);
+    if (EFI_ERROR (Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Failed to get memory map for exiting boot services - Status : %r\n",Status));
+        return Status;
+    }
+
+    Status = gBS->ExitBootServices (
+            ImageHandle,
+            Info->MapKey
+        );
+    if (EFI_ERROR (Status))
+    {
+        DEBUG ((DEBUG_ERROR ,"[FAIL] Failed to exit boot services - Status : %r\n",Status));
+        return Status;
+    }
+    Info->MapCount = Info->MapSiz / Info->DescSiz;
+
+    return Status;
+}
 
 EFI_STATUS EFIAPI UefiMain (
         IN EFI_HANDLE        ImageHandle,
@@ -25,8 +57,11 @@ EFI_STATUS EFIAPI UefiMain (
     EFI_PHYSICAL_ADDRESS KernelEntry;
     KernelLoad (L"\\Kernel.elf",&KernelEntry);
 
+    MAP_INFO Map;
+    ExitBootServices (ImageHandle,&Map);
+
     UINT64 Ret = ((UINT64 (*)(void))KernelEntry)(); // A ptr to entry and call it to get status it returned
-    DEBUG ((DEBUG_INFO ,"[INFO] Kernel returned : %llu\n",Ret));
+    IGNORE (Ret);
 
     return EFI_SUCCESS;
 }
