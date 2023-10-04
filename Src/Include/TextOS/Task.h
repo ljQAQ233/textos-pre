3ba20538658239aea4f70a709a9df0003dbbf32a
Maouai233 <maouai233@outlook.com>
2023-10-04 11:25:56 +0800

    [bili] 042 任务睡眠

--- Src/Include/TextOS/Task.h
+++ Src/Include/TextOS/Task.h
@@ -13,22 +13,29 @@ typedef struct {
     u64  rip;
 } TaskFrame_t;
 
+#include <TextOS/Lib/List.h>
+
 typedef struct _Task {
     int Pid;
     int Stat;
     u64 Tick;
     u64 Curr;
+    
+    u64 Sleep;
 
     struct {
         void *Main;
     } Init;
 
     TaskFrame_t *Frame;
+
+    List_t Sleeper;
 } Task_t;
 
 #define TASK_DIE  0 // Dead
 #define TASK_PRE  1 // Prepared
 #define TASK_RUN  2 // Running
+#define TASK_SLP  3 // Sleep
 
 void TaskSwitch ();
 
@@ -37,5 +44,7 @@ void TaskYield ();
 Task_t *TaskCurr ();
 
 Task_t *TaskCreate (void *Main);
+
+void TaskSleep (u64 Ticks);
 
 #endif
--- Src/Kernel/Task.c
+++ Src/Kernel/Task.c
@@ -2,12 +2,20 @@
 #include <TextOS/Task.h>
 #include <TextOS/Memory/VMM.h>
 
+#include <TextOS/Assert.h>
+
 #include <string.h>
+
+/* TODO : Make it included by Kernel proc */
+struct _LIST _Sleeper;
 
 #define TASK_MAX  16
 
 #define TASK_PAGE (1)
 #define TASK_SIZ  (PAGE_SIZ * TASK_PAGE)
+
+/* Each task has this def tick - 1 */
+#define TASK_TICKS 1
 
 Task_t *Table[TASK_MAX];
 
@@ -64,6 +72,9 @@ Task_t *TaskCreate (void *Main)
     Task->Frame = Frame;
     Task->Stat  = TASK_PRE;
 
+    Task->Tick = TASK_TICKS;
+    Task->Curr = TASK_TICKS;
+
     return Task;
 }
 
@@ -99,17 +110,31 @@ Task_t *TaskCurr ()
 
 extern void __TaskSwitch (TaskFrame_t *Next, TaskFrame_t **Curr);
 
+void _UpdateSleep ();
+
 void TaskSwitch ()
 {
-    Task_t *Curr = TaskCurr(); /* Get curr first becase _TaskNext() will change `_Curr` */
+    Task_t *Curr, *Next;
+
+    Curr = TaskCurr(); /* Get curr first becase _TaskNext() will change `_Curr` */
     if (!Curr)
         return;
+    
+    _UpdateSleep();
+    
+    if (Curr->Stat != TASK_RUN)   // If it is not running now, then sched
+        goto Sched;               // Because a running task may have some time ticks
+    if (Curr->Curr-- == 0)        // Update time ticks, if it is zero after this time, set it to origin ticks
+        Curr->Curr = Curr->Tick;  // Recovery -> gain ticks again
+    else
+        return;                   // It hasn't ran out of all ticks yet, make it run again
 
-    Task_t *Next = _TaskNext();
-    if (!Next)
+Sched:
+    if (!(Next = _TaskNext()))
         return;
 
-    Curr->Stat = TASK_PRE;
+    if (Curr->Stat == TASK_RUN)   // The current task may be a sleeping task
+        Curr->Stat = TASK_PRE;    // Only the running task which will be switched out
     Next->Stat = TASK_RUN;
 
     __TaskSwitch (Next->Frame, &Curr->Frame);
@@ -117,6 +142,43 @@ void TaskSwitch ()
 
 void TaskYield ()
 {
+    TaskSwitch();
+}
+
+void _InsertSleep (Task_t *Task)
+{
+    ListInsert (&_Sleeper, &Task->Sleeper);
+}
+
+void _UpdateSleep ()
+{
+    if (ListEmpty (&_Sleeper)) // 没有任务在睡觉呢...
+        return;
+
+    for (List_t *l = _Sleeper.Back; l != &_Sleeper ;l = l->Back) {
+        Task_t *Task = CR (l, Task_t, Sleeper);
+        if (Task->Pid == _Curr) {
+            continue;
+        }
+        if (--Task->Sleep == 0) {   // 坑死老子啦！2的64次方的大整数是什么 O.o
+            Task->Stat = TASK_PRE;
+            ListRemove (l);
+        }
+    }
+}
+
+void TaskSleep (u64 Ticks)
+{
+    ASSERTK (Ticks != 0); // 专门防止 24h 工作制
+
+    Task_t *Curr = TaskCurr();
+    ASSERTK (Curr != NULL);
+
+    Curr->Stat = TASK_SLP;
+    Curr->Sleep = Ticks;
+
+    _InsertSleep (Curr);
+
     TaskSwitch();
 }
 
@@ -129,6 +191,7 @@ void ProcA ()
     while (true) {
         UNINTR_AREA ({
             PrintK ("A");
+            TaskSleep (1);
         });
     }
 }
@@ -138,6 +201,7 @@ void ProcB ()
     while (true) {
         UNINTR_AREA ({
             PrintK ("B");
+            TaskSleep (5);
         });
     }
 }
@@ -152,6 +216,9 @@ void TaskInit ()
     _TaskKernel();
 
     _Curr = 0;
+
+    /* _Sleeper 迭代 停止于其本身, 而不对空指针作检查 */
+    ListInit (&_Sleeper);
 
     TaskCreate (ProcA);
     TaskCreate (ProcB);
